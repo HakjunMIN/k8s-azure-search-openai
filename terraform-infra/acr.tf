b110e72febcbac7606ffe4ae14060d6de5d79f22
Andrew Min <andrewmin@microsoft.com>
2024-09-11 09:35:23 +0900

    remove terraform

--- terraform-infra/acr.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_container_registry" "acr" {
-  name                = "${var.registry_name}${resource.random_id.random.hex}"
-  resource_group_name = azurerm_resource_group.k8s-rg.name
-  location            = azurerm_resource_group.k8s-rg.location
-  sku                 = "Premium"
-  admin_enabled       = true
-}
